9fd9f2de27c75c9683c7e821234b17dd006f2ae2
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2025-10-10 16:33:52 +0300

    Update lab 3 p 4 normal.js

--- lab 3 p 4 normal.js
+++ lab 3 p 4 normal.js
@@ -1,25 +1,5 @@
 'use strict';
 
-const iface = {
-  m1: x => [x],
-  m2: function (x, y) {
-    return [x, y];
-  },
-  m3(x, y, z) {
-    return [x, y, z];
-  }
-}
-
-const result = Object.keys(iface)
-    .filter(key => typeof iface[key] === 'function')
-    .map(key => [key, iface[key].length])
-    
-console.log(result)
-console.log(JSON.stringify(result));
-console.table((result));
-
-
-// second one
 const iface1 = {
   m1: x1 => [x1],
   m2: function (x, y) {
@@ -42,4 +22,5 @@ const methods = (iface1) => {
 };
 console.log(iface1)
 console.log(JSON.stringify(methods(iface1)));
+
 console.table(methods(iface1));
