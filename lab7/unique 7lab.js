a4ca4bb6118452f76210f04f7b6210cd5111924c
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2025-12-03 22:09:52 +0200

    Update unique 7lab.js

--- lab7/unique 7lab.js
+++ lab7/unique 7lab.js
@@ -1,9 +1,8 @@
 const unique = (array) => {
-  const result = []; // новий масив для унікальних значень
-
+  const result = []; 
   for (const item of array) {
-    if (!result.includes(item)) { // перевіряємо, чи ще немає такого елемента
-      result.push(item);          // додаємо, якщо його немає
+    if (!result.includes(item)) { 
+      result.push(item);         
     }
   }
 
@@ -12,3 +11,4 @@ const unique = (array) => {
 
 const result = unique([2, 1, 1, 3, 2]);
 console.log(result); 
+
