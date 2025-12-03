731a9c2b450e31fee8847a30ec473ae12344ebff
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2025-12-03 22:43:02 +0200

    Update compose 6lab.js

--- lab6/compose 6lab.js
+++ lab6/compose 6lab.js
@@ -1,42 +1,49 @@
-const compose = (...fns) => {
-  const error = [];
+"use strict";
 
-  const composed = (x) => {
+const compose = (...fns) => {
+  for (const fn of fns) {
+    if (typeof fn !== "function") {
+      throw new Error("You should use only function!");
+    }
+  }
+
+  const errors = [];
+
+  const composed = (value) => {
     try {
       for (let i = fns.length - 1; i >= 0; i--) {
-        if (typeof fns[i] !== "function") {
-          throw new Error("You should use only function!");
-        }
-        x = fns[i](x);
+        value = fns[i](value);
       }
-      return x;
+      return value;
     } catch (err) {
-      error.forEach((e) => e(err));
+      errors.forEach((e) => e(err));
       return undefined;
     }
   };
 
   composed.on = (event, handler) => {
-    if (event === "error") 
-      error.push(handler);
+    if (event === "error") {
+      errors.push(handler);
+    }
+    return composed;
   };
 
   return composed;
 };
 
-const inc = x => x + 1;        
-const twice = x => x * 2;      
-const cube = x => x**3 ;
+const inc = (x) => x + 1;
+const twice = (x) => x * 2;
+const cube = (x) => x ** 3;
 
-const f1 = compose(inc, twice, cube); 
-console.log(f1(5));
-
+const f1 = compose(inc, twice, cube);
+console.log(f1(5)); 
 
 const f2 = compose(inc, inc, twice);
+console.log(f2(5)); 
 
-console.log(f2(5));
+console.log(compose(inc, 7, cube)(2)); 
 
-console.log(compose(inc, 7, cube)(2));
+
 
 
 
