6216a7be7d9c479efc0339ca8c55330e47acc340
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2025-12-03 21:52:49 +0200

    Update wrapper  8lab.js

--- wrapper  8lab.js
+++ wrapper  8lab.js
@@ -29,6 +29,3 @@ const add = (a, b) => a + b;
 const addNumbers = contract(add, Number, Number, Number);
 console.log(addNumbers(2, 3)); 
 
-const concat = (s1, s2) => s1 + s2;
-const concatStrings = contract(concat, String, String, String);
-console.log(concatStrings('Hello ', 'world!'));
