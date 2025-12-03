c24b82e5d2a80ed52538b28c3def1c60dab9919f
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2025-12-03 22:04:11 +0200

    Update 6-matrix 4lab.js

--- 4lab/6-matrix 4lab.js
+++ 4lab/6-matrix 4lab.js
@@ -1,19 +1,11 @@
 'use strict'
-const max = (matrix) => {
-  let maxValue = matrix[0][0];
 
-  for (let i = 0; i < matrix.length; i++) {
-    const row = matrix[i];
+const arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+const flatArray = arr.flat();
 
-    for (let j = 0; j < row.length; j++) {
-      if (row[j] > maxValue) {
-        maxValue = row[j];
-      }
-    }
-  }
-
-  return maxValue;
-};
-
-const m = max([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+const m = Math.max(...flatArray);
 console.log(m);
