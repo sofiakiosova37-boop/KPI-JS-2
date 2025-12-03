ba9c470bb1582e44603026aa96452f9dd9408cb7
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2025-12-03 22:12:01 +0200

    Update wrapper  8lab.js

--- lab8/wrapper  8lab.js
+++ lab8/wrapper  8lab.js
@@ -4,7 +4,7 @@ const contract = (fn, ...types) => {
     for (let i = 0; i < types.length - 1; i++) {
       if (typeof args[i] !== types[i].name.toLowerCase()) {
         throw new TypeError(
-          `Argumen type should be ${types[i].name}`
+          `Argument type should be ${types[i].name}`
         );
       }
     }
@@ -28,7 +28,3 @@ const contract = (fn, ...types) => {
 const add = (a, b) => a + b;
 const addNumbers = contract(add, Number, Number, Number);
 console.log(addNumbers(2, 3)); 
-
-const concat = (s1, s2) => s1 + s2;
-const concatStrings = contract(concat, String, String, String);
-console.log(concatStrings('Hello ', 'world!'));
