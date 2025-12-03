6582f81c67301ce78c264f4c44232c70dc965c27
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2025-12-03 21:49:28 +0200

    Update elements 7lab.js

--- elements 7lab.js
+++ elements 7lab.js
@@ -12,3 +12,10 @@ const arr = [1, 2, 3, 2, 4, 5];
 removeElements(arr, 2, 4);
 
 console.log(arr); 
+
+const array = ["Kiev", "Beijing", "Lima", "Saratov"];
+
+removeElements(array,"Kiev", "Berlin");
+
+console.log(array); 
+
